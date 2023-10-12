1efe32d25dbf32b9d2d740e8eccb63d3c35d0a22
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-10-12 21:50:47 +0200

    Work with nested dynamic routes

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,7 @@
+const ClientsProductPage = () => (
+  <div>
+    <h1>The projects of a given Client</h1>
+  </div>
+);
+
+export default ClientsProductPage;
---
+++ pages/clients/index.js
@@ -0,0 +1,8 @@
+const ClientsPage = () => (
+    <div>
+      <h1>This is Clients Page</h1>
+    </div>
+  );
+  
+  export default ClientsPage;
+  
