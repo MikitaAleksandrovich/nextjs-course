b04646e4bceb37a5922f3bb2d8bf7bb3a091a8dd
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-10-20 13:36:02 +0200

    Work on rounting part 2

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+const NotFoundPage = () => (
+  <div>
+    <h1>Page not found</h1>
+  </div>
+);
+
+export default NotFoundPage;
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,10 +1,18 @@
-const ClientsProductPage = () => (
-  <div>
-    <h1>The projects of a given Client</h1>
-    <button>
-      
-    </button>
-  </div>
-);
+import { useRouter } from "next/router";
+
+const ClientsProductPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    router.push("/clients/max/projectA");
+  };
+
+  return (
+    <div>
+      <h1>The projects of a given Client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
+    </div>
+  );
+};
 
 export default ClientsProductPage;
