e4f0d3f7a3ce5a9f4ea850619cf0bc6bd3f08631
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-11-05 16:45:03 +0100

    Fetch data and render on server

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
-const HomePage = () => {
+import fs from "fs/promises";
+import path from "path";
+
+const HomePage = (props) => {
+  const { products } = props;
   return (
     <div>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
       </ul>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
 
 export default HomePage;
