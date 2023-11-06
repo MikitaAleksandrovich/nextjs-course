e5632fda1a2be7cf7a311710d0b050964274799a
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-11-06 20:32:28 +0100

    work with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,30 @@ const HomePage = (props) => {
 };
 
 export async function getStaticProps() {
+  console.log("regenerate");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
